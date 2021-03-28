9b19b3703bf5bd60a124e5df3f23bf37715ed3dd
Bourke Floyd <chbfiv@gmail.com>
2021-03-28 08:59:23 -0700

    removed entrypoint for now

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ set -o pipefail
 if [[ ("$1" == "dev") || ("$1" == "slim") ]]; then
   IMAGE="cardano-node-iohk-$1"
 else
-  printf "please select an option (cardano node): dev or slim"
+  printf "please select an option (cardano-node): dev or slim"
   exit 1
 fi
 
